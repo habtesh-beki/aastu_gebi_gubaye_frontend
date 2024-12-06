b31fe8632a3d7bde1fba747d0adf87aba5d5e9d0
kaleab tadesse <kaleabtadesse49@gmail.com>
2024-12-06 15:59:20 +0300

    updated folder structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { Button } from "@/components/ui/button";
+import { Button } from "@/shared/components/ui/button";
 
 function App() {
   return <Button className="bg-blue-500">Click Me</Button>;

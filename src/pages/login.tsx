fc53181615e003a71bf4f9140914de3bdb405cee
Habtemariam <habteshbaki2014@gmail.com>
2024-12-07 03:40:08 -0800

    I have done login and Add student page

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -2,7 +2,7 @@ import Login from "@/shared/components/Login/login";
 
 export default function LoginPage(){
     return (
-        <div className="flex justify-center items-center bg-bg_login h-screen w-full">
+        <div className="flex bg-[#ECF2FF] justify-center items-center bg-white h-screen w-full">
          <Login/>
         </div>
     )

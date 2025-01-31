d0544f2da7cae13ade6ad161105cb610d376ed86
kaleab tadesse <kaleabtadesse49@gmail.com>
2025-01-31 11:52:47 +0300

    restructed Filter compoent to work with shadcn dialog component

--- src/features/Filter/components/Footer.tsx
+++ src/features/Filter/components/Footer.tsx
@@ -1,8 +1,8 @@
 import { Button } from "@/shared/components/ui/button";
 
-export default function FilterFooter() {
+export default function Footer() {
   return (
-    <div className="flex justify-between items-center gap-2 pl-9 pr-9 p-4 h-20 bg-bg_login mt-auto">
+    <div className="w-full flex justify-between items-center gap-2 bg-bg_login">
       <Button className="text-red-600 w-2/3  border" variant={"outline"}>
         Cancel
       </Button>

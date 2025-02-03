86dfc2650abf73bbc3090b3fe3e110f0f7731144
Habtemariam <habteshbaki2014@gmail.com>
2025-02-02 22:18:11 -0800

    add comment in protected route

--- src/shared/utils/protectedRoute.tsx
+++ src/shared/utils/protectedRoute.tsx
@@ -1,5 +1,5 @@
 import { Navigate, Outlet } from "react-router";
-
+////protect access other route before logedIn
 export function ProtectedRoute({ logedIn }: { logedIn: boolean }) {
   return logedIn ? <Outlet /> : <Navigate to="/login" />;
 }

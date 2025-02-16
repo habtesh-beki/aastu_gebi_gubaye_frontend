4beb943a353c7e836c91ed8da9f0f38f7047efdb
Habtemariam <habteshbaki2014@gmail.com>
2025-02-16 11:26:24 -0800

    add some changes for detailInfo

--- src/features/DetailInfo/components/Footer.tsx
+++ src/features/DetailInfo/components/Footer.tsx
@@ -1,12 +1,43 @@
+import {
+    InnerDialog,
+    InnerDialogContent,
+    InnerDialogFooter,
+    InnerDialogHeader,
+    InnerDialogTrigger,
+} from "@/components/ui/dialog";
+import {
+    WarningBody,
+    WarningFooter,
+    WarningHeader,
+} from "@/features/Warning/Warning";
 import { Button } from "@/shared/components/ui/button";
+import {
+    Dialog,
+    DialogContent,
+    DialogFooter,
+    DialogHeader,
+    DialogTrigger,
+} from "@/shared/components/ui/dialog";
 
 export default function Footer() {
-  return (
-    <div className="w-full flex justify-between items-center gap-2 bg-bg_login">
-      <Button className="text-red-600 hover:text-white w-2/3 bg-transparent hover:bg-red-600 border-2 hover:border-[0px] border-solid border-red-600">
-        Remove
-      </Button>
-      <Button className="bg-bg_btn hover:bg-blue-600 w-2/3">Edit</Button>
-    </div>
-  );
+    return (
+        <div className="w-full flex justify-between items-center gap-2 bg-bg_login">
+            <Dialog>
+                <DialogTrigger className="text-red-600 hover:text-white w-1/2 bg-transparent hover:bg-red-600 border-2 hover:border-[0px] border-solid border-red-600 p-2 rounded-md">
+                    Remove
+                </DialogTrigger>
+                <DialogContent className="flex bg-white flex-col items-strech justify-between  max-w-[460px] absolute top-1/4  border rounded-lg overflow-hidden shadow-lg">
+                    <DialogHeader>
+                        <WarningHeader />
+                    </DialogHeader>
+                    <WarningBody />
+                    <DialogFooter>
+                        <WarningFooter />
+                    </DialogFooter>
+                </DialogContent>
+            </Dialog>
+
+            <Button className="bg-bg_btn hover:bg-blue-600 w-1/2">Edit</Button>
+        </div>
+    );
 }

540b2b9c7f4aba51ac439eb4a60ec423ce75f6d2
Habtemariam <habteshbaki2014@gmail.com>
2025-02-16 11:31:02 -0800

    added my profile route and my profile page

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,41 +1,41 @@
 // routes.ts
 import { RouteObject, Navigate } from "react-router";
 import Dashboard from "./features/dashboard/Dashboard";
-import AddConfession from "./features/Confession/AddConfession/AddConfession";
 import AllMembers from "@/features/AllMembers/AllMembers";
 import Admins from "@/features/Admins/Admins";
-import UpdateStudent from "@/features/UpdateStudent/UpdateStudent";
 import { FetchParamsProvider } from "@/features/AllMembers/context/filterParamContex";
+import Message from "./features/Messageing/Message";
+import MyProfile from "./features/MyProfile/MyProfile";
 
 const routes: RouteObject[] = [
-  {
-    path: "dashboard",
-    element: <Dashboard />,
-  },
-  {
-    path: "admins",
-    element: <Admins />,
-  },
-  {
-    path: "all-members",
-    element: (
-      <FetchParamsProvider>
-        <AllMembers />
-      </FetchParamsProvider>
-    ),
-  },
-  {
-    path: "message",
-    element: <UpdateStudent />,
-  },
-  {
-    path: "my-profile",
-    element: <AddConfession />,
-  },
-  {
-    path: "*",
-    element: <Navigate to="/dashboard" replace />,
-  },
+    {
+        path: "dashboard",
+        element: <Dashboard />,
+    },
+    {
+        path: "admins",
+        element: <Admins />,
+    },
+    {
+        path: "all-members",
+        element: (
+            <FetchParamsProvider>
+                <AllMembers />
+            </FetchParamsProvider>
+        ),
+    },
+    {
+        path: "message",
+        element: <Message />,
+    },
+    {
+        path: "my-profile",
+        element: <MyProfile />,
+    },
+    {
+        path: "*",
+        element: <Navigate to="/dashboard" replace />,
+    },
 ];
 
 export default routes;

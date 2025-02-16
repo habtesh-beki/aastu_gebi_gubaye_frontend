4efb6004f65a766eec84d5ae584a58bf7894f7e2
Habtemariam <habteshbaki2014@gmail.com>
2025-02-16 11:28:53 -0800

    refactor Warning page

--- src/features/Warning/components/Footer.tsx
+++ src/features/Warning/components/Footer.tsx
@@ -1,14 +1,14 @@
 import { Button } from "@/shared/components/ui/button";
 
 export default function Footer() {
-  return (
-    <div className="h-20 bg-bg_login flex items-center justify-center gap-x-4">
-      <Button className=" w-32 text-black border" variant={"secondary"}>
-        Cancel
-      </Button>
-      <Button className="w-32" variant={"destructive"}>
-        Remove
-      </Button>
-    </div>
-  );
+    return (
+        <div className="h-20 bg-bg_login flex items-center justify-center gap-x-4 w-full p-1">
+            <Button className="w-1/2  text-black border" variant={"secondary"}>
+                Cancel
+            </Button>
+            <Button className="w-1/2" variant={"destructive"}>
+                Remove
+            </Button>
+        </div>
+    );
 }
---
+++ src/features/Warning/components/WarningBody.tsx
@@ -0,0 +1,9 @@
+export default function WarningBody() {
+    return (
+        <div className="flex flex-col items-center justify-center p-10">
+            <p className="text-black text-2xl text-center">
+                Are you sure you want to remove the student !
+            </p>
+        </div>
+    );
+}

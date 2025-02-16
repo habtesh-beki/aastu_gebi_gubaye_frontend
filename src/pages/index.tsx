f9e58d1d20af3a1e490a1d168988ce7a143ff686
Habtemariam <habteshbaki2014@gmail.com>
2025-02-16 11:30:07 -0800

    added my profile route

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,39 +4,39 @@ import { Sidebar } from "@/shared/components";
 import { Outlet } from "react-router";
 
 export default function HomePage() {
-  return (
-    <div className="bg-[#dbeafe]">
-      <Navbar style="fixed left-0 right-0 top-0 z-10" />
-      <main className="flex h-screen">
-        <Sidebar
-          items={[
-            {
-              name: "Dashboard",
-              icon: LayoutDashboard,
-              path: "dashboard",
-            },
-            {
-              name: "Admins",
-              icon: Shield,
-              path: "admins",
-            },
-            {
-              name: "All members",
-              icon: Users,
-              path: "all-members",
-            },
-            {
-              name: "Message",
-              icon: Mail,
-              path: "message",
-            },
-          ]}
-          style="pt-32"
-        />
-        <div className="w-full overflow-y-scroll">
-          <Outlet />
+    return (
+        <div className="bg-[#dbeafe]">
+            <Navbar style="fixed left-0 right-0 top-0 z-10" />
+            <main className="flex h-screen">
+                <Sidebar
+                    items={[
+                        {
+                            name: "Dashboard",
+                            icon: LayoutDashboard,
+                            path: "dashboard",
+                        },
+                        {
+                            name: "Admins",
+                            icon: Shield,
+                            path: "admins",
+                        },
+                        {
+                            name: "All members",
+                            icon: Users,
+                            path: "all-members",
+                        },
+                        {
+                            name: "Message",
+                            icon: Mail,
+                            path: "message",
+                        },
+                    ]}
+                    style="pt-32"
+                />
+                <div className="w-full overflow-y-scroll">
+                    <Outlet />
+                </div>
+            </main>
         </div>
-      </main>
-    </div>
-  );
+    );
 }

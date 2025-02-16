7641d1dbd5ada54a8152d9cb6d212664a76a4c30
Habtemariam <habteshbaki2014@gmail.com>
2025-02-16 11:23:45 -0800

    add message page

---
+++ src/features/Messageing/Message.tsx
@@ -0,0 +1,9 @@
+export default function Message() {
+    return (
+        <div className="flex justify-center items-center h-screen bg-transparent">
+            <div className="text-6xl font-bold bg-gradient-to-r from-blue-500 to-purple-500 bg-clip-text text-transparent animate-pulse">
+                Coming Soon ...
+            </div>
+        </div>
+    );
+}

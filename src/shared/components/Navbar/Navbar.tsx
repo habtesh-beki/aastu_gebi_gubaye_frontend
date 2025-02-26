4ebca7b3b4a04d6b48d895c694ba0c1b2ae77833
Habtemariam <habteshbaki2014@gmail.com>
2025-02-26 06:02:24 -0800

    change username of logedin by real username

--- src/shared/components/Navbar/Navbar.tsx
+++ src/shared/components/Navbar/Navbar.tsx
@@ -1,8 +1,45 @@
 import { NavLink } from "react-router";
 import { Separator } from "@/shared/components/ui/separator";
 import LOGO from "@/assets/logo.png";
+import { useEffect, useState } from "react";
+import axios from "axios";
+
+type Istudent = {
+    id: string;
+    first_name: string;
+    last_name: string;
+    baptismal_name?: string;
+    gender: string;
+    student_id: string;
+    department: { id: string; department: string };
+    service?: { id: string; name: string }[];
+    language: { id: string; name: string }[];
+    current_year: string;
+    password: string;
+    confession?: string;
+    role: string;
+    phone_number: string;
+    email?: string;
+};
 
 export default function Navbar({ style }: { style?: string }) {
+    const [student, setStudent] = useState<Istudent>();
+    const token = localStorage.getItem("auth-token");
+    useEffect(() => {
+        axios
+            .get("http://127.0.0.1:3000/api/student/logedin/person", {
+                headers: {
+                    Authorization: `Bearer ${token}`,
+                },
+            })
+            .then((response) => {
+                console.log(response.data.data);
+                setStudent(response.data.data);
+            })
+            .catch((err) => {
+                console.log(err);
+            });
+    }, [token]);
     return (
         <nav
             className={`p-4 bg-background flex justify-between shadow-md ${style}`}
@@ -33,11 +70,14 @@ export default function Navbar({ style }: { style?: string }) {
                 />
                 <div className="flex items-center gap-4">
                     <div className="w-12 h-12 text-lg text-white font-extrabold bg-[#3D63DD] rounded-full flex items-center justify-center">
-                        ST
+                        {student?.first_name.slice(0, 1)}
+                        {student?.last_name.slice(0, 1)}
                     </div>
                     <div>
-                        <p className="text-lg font-medium">Solomon Tarekegn</p>
-                        <p className="text-[#8C8C98]">Super Admin</p>
+                        <p className="text-lg font-medium">
+                            {student?.first_name} {student?.last_name}
+                        </p>
+                        <p className="text-[#8C8C98]">{student?.role}</p>
                     </div>
                 </div>
             </div>

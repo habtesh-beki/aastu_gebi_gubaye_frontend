acbdc156586ed2a97966c5fcf341cd9ea13e0e78
kaleab tadesse <kaleabtadesse49@gmail.com>
2025-02-26 11:48:05 +0300

    integrate the server-side pagination to all members table

--- src/features/AllMembers/AllMembers.tsx
+++ src/features/AllMembers/AllMembers.tsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import { columns } from "@/features/AllMembers/components/columns";
 import { DataTable } from "@/shared/components/DataTable/DataTable";
 import { FilterMembers } from "./components/FilterMembers";
@@ -5,7 +6,11 @@ import { FilterMembers } from "./components/FilterMembers";
 import useGetStudents from "@/shared/hooks/useGetStudents";
 
 export default function AllMembers() {
-    const { isPending, error, data } = useGetStudents();
+    const [pagination, setPagination] = useState({
+        pageIndex: 0,
+        pageSize: 10,
+    });
+    const { isPending, error, data } = useGetStudents(pagination);
 
     return (
         <div className="container mx-auto pt-32 pb-12 px-8 space-y-12">
@@ -15,7 +20,7 @@ export default function AllMembers() {
                     Loading Student Data
                 </div>
             ) : !error ? (
-                <DataTable columns={columns} data={data} />
+                <DataTable columns={columns} data={data.data} needsPagination={true} pagination={pagination} setPagination={setPagination} pageNumber={Math.ceil(data.total / pagination.pageSize)} />
             ) : (
                 <div className="w-full py-6 text-center bg-white rounded-xl overflow-hidden">
                     {error.toString()}

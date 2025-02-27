626cb888aa7fc6949ec122520e2bca064ca916ce
Habtemariam <habteshbaki2014@gmail.com>
2025-02-27 02:10:49 -0800

    merge with remote branch

--- src/shared/hooks/useGetConfessionFathers.tsx
+++ src/shared/hooks/useGetConfessionFathers.tsx
@@ -3,43 +3,44 @@ import axios from "axios";
 import { Data } from "@/features/Admins/components/ConfessionFatherColumns";
 
 export default function useGetConfessionFathers() {
+    type ConfessionFather = {
+        id: string;
+        first_name: string;
+        last_name: string;
+        phoneNumber: string;
+    };
 
-  type ConfessionFather = {
-    id: string;
-    first_name: string;
-    last_name: string;
-    phoneNumber: string;
-  };
+    const token = localStorage.getItem("auth-token");
 
-  const token = localStorage.getItem("auth-token");
+    return useQuery({
+        queryKey: ["confessionFather"],
+        queryFn: async (): Promise<Data[]> => {
+            const options = {
+                method: "GET",
+                url: "http://localhost:3000/api/confession",
+                headers: {
+                    Authorization: `Bearer ${token}`,
+                },
+            };
 
-  return useQuery({
-    queryKey: ["confessionFather"], queryFn: async (): Promise<Data[]> => {
-
-      const options = {
-        method: "GET",
-        url: "http://localhost:3000/api/confession",
-        headers: {
-          Authorization: `Bearer ${token}`,
-        },
-      };
-
-      let response;
-      try {
-         response = await axios.request(options).then((data) => {
-          return data.data.data.confession.map((confessionFather: ConfessionFather) => {
-            return {
-                firstName: confessionFather.first_name,
-                lastName: confessionFather.last_name,
-                phoneNumber: confessionFather.phoneNumber
+            let response;
+            try {
+                response = await axios.request(options).then((data) => {
+                    return data.data.data.confession.map(
+                        (confessionFather: ConfessionFather) => {
+                            return {
+                                firstName: confessionFather.first_name,
+                                lastName: confessionFather.last_name,
+                                phoneNumber: confessionFather.phoneNumber,
+                            };
+                        }
+                    );
+                });
+            } catch (error) {
+                response = `Error: ${error}`;
             }
-          });
-        })
-      } catch (error) {
-        response = `Error: ${error}`;
-      }
-      
-      return response;
-    }
-  })
+
+            return response;
+        },
+    });
 }

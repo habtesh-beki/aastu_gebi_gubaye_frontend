33649d1bb1fc0241c390ce70f891c4fe6f470199
kaleab tadesse <kaleabtadesse49@gmail.com>
2025-02-27 10:20:48 +0300

    add remove filer button

--- src/features/AllMembers/components/FilterMembers.tsx
+++ src/features/AllMembers/components/FilterMembers.tsx
@@ -1,7 +1,8 @@
+import { useState } from "react";
 import { useForm, SubmitHandler } from "react-hook-form";
 import { useFetchParams } from "@/features/AllMembers/context/filterParamContex";
 
-import { ListFilter, FileSpreadsheet, Search } from "lucide-react";
+import { FileSpreadsheet, Search, Filter, FilterX } from "lucide-react";
 import {
     Dialog,
     DialogTrigger,
@@ -32,13 +33,20 @@ export function FilterMembers() {
     const { register, reset, handleSubmit, setValue } = useForm<Inputs>({});
     const { setFetchParams } = useFetchParams();
 
+    const [open, setOpen] = useState(false);
+
     const onSubmit: SubmitHandler<Inputs> = (data) => {
         const sort = data.sort === "" ? "first_name" : data.sort;
         const paramData = { ...data, sort };
         setFetchParams(paramData);
+        setOpen(false);
     };
 
     const handleCancel = () => {
+        setOpen(false); 
+    };
+
+    const handleClearFilter = () => {
         setFetchParams({
             page: "1",
             department: "",
@@ -59,12 +67,12 @@ export function FilterMembers() {
             sort: "first_name",
             keyword: "",
         });
-    };
+    }
 
     return (
         <div className="bg-white p-6 rounded-lg space-y-8">
             <h2 className="text-3xl">Students</h2>
-            <div className="flex justify-between flex-col gap-6 min-[1100px]:gap-0 min-[1100px]:flex-row">
+            <div className="flex justify-between flex-col gap-6 min-[1200px]:gap-0 min-[1200px]:flex-row">
                 <div className="w-full lg:w-[400px] bg-blue-50 rounded-lg px-4 py-4 has-[:focus]:ring-blue-700 has-[:focus]:ring-2 has-[:focus]:ring-offset-2 group flex items-center">
                     <input
                         type="text"
@@ -77,10 +85,10 @@ export function FilterMembers() {
                         className="text-gray-300 cursor-pointer group-has-[:focus]:text-gray-600"
                     />
                 </div>
-                <div className="text-blue-700 font-bold space-x-2 flex gap-4">
-                    <Dialog>
+                <div className="text-blue-700 font-bold space-x-2 flex gap-4 items-center">
+                    <Dialog open={open} onOpenChange={setOpen}>
                         <DialogTrigger className="text-md px-10 py-4 bg-blue-100 text-blue-700 flex gap-2 rounded-lg border-[2.5px] border-blue-400 hover:bg-blue-700 hover:text-white hover:border-blue-700 focus-visible:ring-2 focus-visible:ring-blue-700 focus-visible:ring-offset-2">
-                            <ListFilter />
+                            <Filter />
                             Filter
                         </DialogTrigger>
                         <DialogContent className="p-0 overflow-hidden">
@@ -101,6 +109,9 @@ export function FilterMembers() {
                             </DialogFooter>
                         </DialogContent>
                     </Dialog>
+                    <button className="text-md py-4 px-4 text-blue-700 bg-blue-100 rounded-lg border-[2.5px] border-blue-400 hover:bg-blue-700 hover:text-white hover:border-blue-700 focus-visible:ring-2 focus-visible:ring-blue-700 focus-visible:ring-offset-2" onClick={handleClearFilter}>
+                        <FilterX />
+                    </button>
                     <Dialog>
                         <DialogTrigger className="px-6 py-4 bg-blue-100 text-blue-700 rounded-lg border-[2.5px] border-blue-400 hover:bg-blue-700 hover:text-white hover:border-blue-700 focus-visible:ring-2 focus-visible:ring-blue-700 focus-visible:ring-offset-2">
                             <FileSpreadsheet />

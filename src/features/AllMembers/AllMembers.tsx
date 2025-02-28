a594f5167183999a1a8e00413d5b188ca95837ab
Habtemariam <habteshbaki2014@gmail.com>
2025-02-27 22:47:15 -0800

    fix the padding top

--- src/features/AllMembers/AllMembers.tsx
+++ src/features/AllMembers/AllMembers.tsx
@@ -13,14 +13,21 @@ export default function AllMembers() {
     const { isPending, error, data } = useGetStudents(pagination);
 
     return (
-        <div className="container mx-auto pt-32 pb-12 px-8 space-y-12">
+        <div className="container mx-auto pt-10 pb-12 px-8 space-y-12">
             <FilterMembers />
             {isPending ? (
                 <div className="w-full py-6 text-center bg-white rounded-xl overflow-hidden">
                     Loading Student Data
                 </div>
             ) : !error ? (
-                <DataTable columns={columns} data={data.data} needsPagination={true} pagination={pagination} setPagination={setPagination} pageNumber={Math.ceil(data.total / pagination.pageSize)} />
+                <DataTable
+                    columns={columns}
+                    data={data.data}
+                    needsPagination={true}
+                    pagination={pagination}
+                    setPagination={setPagination}
+                    pageNumber={Math.ceil(data.total / pagination.pageSize)}
+                />
             ) : (
                 <div className="w-full py-6 text-center bg-white rounded-xl overflow-hidden">
                     {error.toString()}
